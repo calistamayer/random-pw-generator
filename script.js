a8b528ac5cae87fb4e39d75bf7a2862d83f20850
Calista Mayer <calista.a.mayer@gmail.com>
2020-10-04 19:16:07 -0700

    prompt for length between 8 and 128 characters

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Assignment code here
-window.prompt
+
+// presents length prompt
+var length = function() {
+  var promptLength = window.prompt('How many characters long will your password be? Enter a value between 8 and 128.');
+  if (promptLength < 8 || promptlength > 128) {
+    window.prompt('Please enter a value between 8 and 128.');
+  }
+}
+
+length();
 
 
 // Get references to the #generate element
